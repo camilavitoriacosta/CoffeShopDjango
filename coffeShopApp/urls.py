98019154044701824ed732bcd375b30d58edcd4e
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2021-12-18 14:51:59 -0400

    Ajeitando urls para filtrar catalogo de acordo com a categoria

--- coffeShopApp/urls.py
+++ coffeShopApp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.pagina_inicial,name='pagina_inicial'),
-    path('catalogo_produtos', views.catalogo_produtos, name='catalogo_produtos')
+    path('catalogo_produtos/coffe', views.catalogo_produtos_coffe, name='catalogo_produtos_coffe'),
+    path('catalogo_produtos/tea', views.catalogo_produtos_tea, name='catalogo_produtos_tea'),
+    path('catalogo_produtos/smoothie', views.catalogo_produtos_smoothie, name='catalogo_produtos_smoothie')
 ]
--- coffeShopApp/views.py
+++ coffeShopApp/views.py
@@ -5,16 +5,32 @@ def pagina_inicial(requisicao):
     return render(requisicao, 'pagina_inicial.html')
 
 
-def catalogo_produtos(requisicao):
-    #inserir_produto(requisicao)
-    
-    produtos = Produto.objects.all()
+def catalogo_produtos_coffe(requisicao):
+    produtos = filtrar_produto_por_categoria("coffe")    
     dados = {
         'produtos': produtos
     }
     
     return render(requisicao, 'catalogo_produtos.html', dados)
 
+def catalogo_produtos_tea(requisicao):
+    produtos = filtrar_produto_por_categoria("tea")    
+    dados = {
+        'produtos': produtos
+    }
+    
+    return render(requisicao, 'catalogo_produtos.html', dados)
+
+def catalogo_produtos_smoothie(requisicao):
+    produtos = filtrar_produto_por_categoria("smoothie")    
+    dados = {
+        'produtos': produtos
+    }
+    return render(requisicao, 'catalogo_produtos.html', dados)
+
+def filtrar_produto_por_categoria(categoria_produto):
+    return Produto.objects.filter(categoria=categoria_produto)
+    
 def inserir_produto(requisicao):
     produto = Produto(nome_produto="Capuccino", descricao= "Cafe com leite", preco=0.50, categoria="Coffe")
     produto.save()
